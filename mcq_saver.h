1e2c833c8c2c0ade3fb1d7ff2a644cf4876d4104
Shin-Senpai <jarif040905@gmail.com>
2024-02-06 21:24:17 +0600

    Added a header file containing forward declarationn for mcq_saver()

---
+++ mcq_saver.h
@@ -0,0 +1,11 @@
+#ifndef mcq_saver_h
+#define mcq_saver_h
+
+// Dependencies
+#include "struct_mcq.h"
+#include <fstream>
+
+//Forward declared dependencies
+void mcq_saver(std::vector<MCQ> test);
+
+#endif
