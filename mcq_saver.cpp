14ba6369c79a8db65fe573b56bfff54fe019ae51
Shin-Senpai <jarif040905@gmail.com>
2024-02-06 20:36:11 +0600

    A new source file containing a function to save files to disk

---
+++ mcq_saver.cpp
@@ -0,0 +1,38 @@
+#include "struct_mcq.h"
+#include <iostream>
+#include <fstream>
+
+void mcq_saver(std::vector<MCQ> test)
+{
+	// Open a file for writing
+	std::cout << "Enter the MCQ set name: ";
+	std::string filename{};
+	std::getline(std::cin >> std::ws, filename);
+	std::ofstream outputFile(filename + ".txt");
+
+	// Check if the file is opened successfully
+	if (outputFile.is_open()) {
+
+		// Serialize and write each MCQ object to the file
+		for (const auto& question : test) {
+			outputFile << "Question: " << question.questionText << "\n\n";
+		
+			int option_num{ 0 };
+			
+			outputFile << "Options:" << "\n";
+			for (const auto& option : question.options) {
+				option_num++;
+				outputFile << option_num << '.' << option << "\n";
+			}
+			
+			outputFile << "\nAnswer: " << question.answer << "\n";
+		}
+
+		// Close the file
+		outputFile.close();
+		std::cout << "\nMCQ test has been saved to mcq_test.txt\n";
+	}
+	else {
+		std::cerr << "Unable to open file for writing.\n";
+	}
+}
